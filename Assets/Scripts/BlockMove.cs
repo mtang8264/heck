bb5dcb3f30a29359b8d0d469dfb352e433cbb1bf
Michael Tang <mtang8264@gmail.com>
2018-04-21 08:51:57 -0400

    v0.0.2.1
    Added a more complex block pattern for testing

--- Assets/Scripts/BlockMove.cs
+++ Assets/Scripts/BlockMove.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BlockMove : MonoBehaviour {
+	public bool move = false;
 	public GameObject[] BlockOptions;
 	private Transform heck;
+	public float dist = 10f;
 
 	// Use this for initialization
 	void Awake () {
@@ -13,9 +15,11 @@ public class BlockMove : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (heck.position.y > transform.position.y + 10f) {
+		if (heck.position.y > transform.position.y + dist) {
 			int selection = Random.Range (0, BlockOptions.Length);
-			Instantiate(BlockOptions[selection], transform.position + new Vector3(0f,40f),new Quaternion());
+			if (move) {
+				Instantiate (BlockOptions [selection], transform.position + new Vector3 (0f, 40f), new Quaternion ());
+			}
 			Destroy (gameObject);
 		}
 	}
---
+++ Assets/Scripts/BlockSpawner.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockSpawner : MonoBehaviour {
+	public float lastSpawn = 10f;
+	public GameObject[] blockOptions;
+	private Transform heck;
+
+	// Use this for initialization
+	void Start () {
+		heck = GameObject.FindWithTag ("Heck").GetComponent<Transform> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = heck.position + new Vector3 (0f, 20f, 0f);
+
+		if (lastSpawn + 40f < transform.position.y) {
+			Debug.Log ("Could spawn");
+			lastSpawn = transform.position.y;
+			bool decision = Random.Range (-1f, 1f) > 0;
+			if (decision)
+				Spawn ();
+		}
+	}
+
+	void Spawn () {
+		int selection = Random.Range (0, blockOptions.Length);
+		bool direction = Random.Range (-1f, 1f) >= 0;
+		if (direction) {
+			Instantiate (blockOptions [selection], transform.position, new Quaternion ()).transform.localScale = new Vector3 (1f, 1f, 1f);
+		} else {
+			Instantiate (blockOptions [selection], transform.position, new Quaternion ()).transform.localScale = new Vector3 (-1f, 1f, 1f);
+		}
+	}
+}
